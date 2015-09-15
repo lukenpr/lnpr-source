8339b66b1fc12a6581d1f1b7329911847df71944
lukenpr <luke.npr@gmail.com>
2015-09-15 17:26:43 +1000

    first commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/lukenpr.com/"
+http_images_path = "/lukenpr.com/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/lukenpr.com/fonts"
+css_dir = "public/lukenpr.com/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
